9fc90e01cb09c13f6b393db60680abb864bdaf6a
Rahul <rahulreghunath11@gmail.com>
2018-10-07 15:22:21 +0530

    Initial Commit

---
+++ src/Demo/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="app">
+        <list :items="items" v-model="value"></list>
+    </div>
+</template>
+
+<script>
+    import('bootstrap/dist/css/bootstrap.css');
+
+    import List from '../SuggestionList/SuggestionList';
+
+    export default {
+        name: 'app',
+        components: {List},
+        data() {
+            return {
+                value: '2',
+                items: [
+                    {option: 'First', value: 1},
+                    {option: 'Two', value: 2},
+                    {option: 'Three', value: 3}
+                ]
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/SuggestionList/SuggestionList.vue
@@ -0,0 +1,86 @@
+<template>
+    <div>
+        <input list="suggestionList" id="answerInput" v-model="suggestionInput" @change="suggestionChanged($event)"/>
+        <datalist id="suggestionList">
+            <option v-for="item in items" :key="item.value" :data-value="item.value">{{ item.option }}</option>
+        </datalist>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SuggestionList",
+        model: {
+            prop: 'value',
+            event: 'change'
+        },
+        props: {
+            value: String,
+            items: Array
+        },
+        data() {
+            return {
+                selected: this.value,
+                suggestionInput: ''
+            }
+        },
+        watch: {
+            selected() {
+                this.$emit('change', this.selected);
+            },
+            suggestionInput() {
+                if (this.suggestionInput === '') {
+                    this.selected = '';
+                }
+            }
+        },
+        methods: {
+            suggestionChanged(e) {
+                if (this.selected !== '') {
+                    let options = document.querySelectorAll('#suggestionList option');
+
+                    for (let i = 0; i < options.length; i++) {
+                        let option = options[i];
+                        if (option.value === e.target.value) {
+                            return;
+                        }
+                    }
+                    this.initialValue();
+                }
+            },
+            initialValue() {
+                let options = document.querySelectorAll('#suggestionList option');
+
+                for (let i = 0; i < options.length; i++) {
+                    let option = options[i];
+                    if (option.getAttribute('data-value') === this.selected) {
+                        document.getElementById('answerInput').value = option.value;
+                        break;
+                    }
+                }
+            }
+        },
+        mounted() {
+            if (this.selected.length) {
+                this.initialValue();
+            }
+            document.querySelector('input[list]').addEventListener('input', (e) => {
+                let options = document.querySelectorAll('#suggestionList option'),
+                    inputValue = e.target.value;
+
+                for (let i = 0; i < options.length; i++) {
+                    let option = options[i];
+
+                    if (option.innerText === inputValue) {
+                        this.selected = option.getAttribute('data-value');
+                        break;
+                    }
+                }
+            });
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
